fe67daec010f779016a238c8287db9e3cd3ba375
Ulises Rdgz <ulises.rdgz@ciencias.unam.mx>
2025-11-17 13:01:12 -0600

    Cambio de url

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -14,11 +14,11 @@ dotenv.config();
 
 export const appConfig = {
   // Entorno y puerto
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'production',
   port: parseInt(process.env.PORT || '5000', 10),
 
-  // Ruta base de la API
-  basePath: process.env.BASE_PATH || '/api',
+  // Ruta base EXACTA del backend detrás de nginx
+  basePath: process.env.BASE_PATH || '/ideamex2/api',
 
   // Información general de la aplicación
   appName: 'IDEAMEX API',
